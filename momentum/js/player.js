80f6ae12430636006180cb76b6773f1f0504b023
Olena-web <73849375+Olena-web@users.noreply.github.com>
2021-10-25 15:45:52 +0300

    feat: add change for playlist

--- momentum/js/player.js
+++ momentum/js/player.js
@@ -2,11 +2,13 @@ const audio = new Audio();
 const playBtn = document.querySelector(".play-main");
 const playNext = document.querySelector(".play-next");
 const playPrev = document.querySelector(".play-prev");
-import playList from "./playList.js";
-console.log(playList);
-let playNum = 0;
-let isPlay = false;
 
+const list = document.querySelector(".play-list");
+const listItem = list.querySelectorAll("span");
+import playList from "./playList.js";
+let playNum = 0;
+
+let isPlay = false;
 function playAudio() {
   audio.src = playList[playNum].src;
   audio.currentTime = 0;
@@ -14,10 +16,12 @@ function playAudio() {
     audio.play();
     isPlay = true;
     toggleBtn();
+    changeList();
   } else {
     audio.pause();
     isPlay = false;
     toggleBtn();
+    changeList();
   }
 }
 
@@ -26,12 +30,47 @@ function toggleBtn() {
 }
 
 playBtn.addEventListener("click", playAudio);
-playNext.addEventListener("click", playAudio);
-playPrev.addEventListener("click", playAudio);
 
-function getNextAudio() {
-  for (let playNum = 0; playNum < playList.length; playNum++) {
-    playAudio();
+playNext.addEventListener("click", () => {
+  if (isPlay) {
+    audio.pause();
+    changeList();
+  }
+  if (playNum == 3) {
+    playNum = 0;
+  } else {
+    playNum++;
+  }
+  audio.src = playList[playNum].src;
+  audio.currentTime = 0;
+  playBtn.classList.add("pause");
+  changeList();
+  audio.play();
+});
+
+playPrev.addEventListener("click", () => {
+  if (isPlay) {
+    audio.pause();
+    changeList();
+  }
+  if (playNum == 0) {
+    playNum = 3;
+  } else {
+    playNum--;
+  }
+  audio.src = playList[playNum].src;
+  audio.currentTime = 0;
+  playBtn.classList.add("pause");
+  changeList();
+  audio.play();
+});
+
+function changeList() {
+  for (let i = 0; i < listItem.length; i++) {
+    if (playNum == i) {
+      listItem[i].classList.toggle("playing");
+      listItem[i].textContent = playList[i].title;
+    }
   }
 }
-audio.addEventListener("ended", getNextAudio);
+
